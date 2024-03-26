755eb9627af2c6f3cd97a9146b8f641d20ecddf4
Maarten van Diemen <maartenvandiemen@msn.com>
2024-03-26 18:29:04 +0100

    Fix build

---
+++ Infrastructure/Datastorage/ShowDbContextFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Showtime.Infrastructure.Datastorage;
+public class ShowDbContextFactory : IDesignTimeDbContextFactory<ShowDbContext>
+{
+    public ShowDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<ShowDbContext>();
+        optionsBuilder.UseSqlServer();
+
+        return new ShowDbContext(optionsBuilder.Options);
+    }
+}
